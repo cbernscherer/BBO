a2d2a18678ca840d9e1e22d1722a94994f965313
Christian Bernscherer <cbernscherer@gmail.com>
2020-04-24 19:02:11 +0200

    Member Upload 1.1

--- memberlist.py
+++ memberlist.py
@@ -14,12 +14,15 @@ def convert(string:str):
 
     return string
 
+# read tables and remove invalid data
 reg_players = pd.read_excel('Spielerliste BBO.xlsx', names=['Nick', 'Name'], usecols=[0, 1])
 reg_players = reg_players[reg_players['Nick'].notna() & reg_players['Name'].notna()]
 
 oebv_players = pd.read_excel('Spielerliste BBO.xlsx', sheet_name='ÖBVSpielerdatei',
                              names=['Zuname', 'Vorname'], usecols=[1, 2])
 oebv_players = oebv_players[oebv_players['Vorname'].notna() & oebv_players['Zuname'].notna()]
+
+# create column for query
 oebv_list = oebv_players[['Vorname', 'Zuname']].values
 names = [' '.join(list(oebv_list[i])).lower() for i in range(len(oebv_list))]
 oebv_players['Name'] = names
@@ -31,13 +34,14 @@ reg_players['CLUB3'] = ''
 reg_players['CLUB4'] = ''
 reg_players['CLUB5'] = ''
 
-
+# identify duplicates
 duplicates = reg_players.duplicated('Name')
 duplicate_entries = reg_players[duplicates]
 reg_players = reg_players[duplicates == False]
 
 occurrencies = defaultdict(lambda: 1)
 
+# join multiple nicks of a player for the import in alias
 for entry in duplicate_entries.itertuples():
     occurrencies[entry.Name] += 1
     if occurrencies[entry.Name] > 5:
@@ -47,6 +51,7 @@ for entry in duplicate_entries.itertuples():
     reg_players.loc[reg_players['Name'] == entry.Name, ["ALIAS"]] = tmp[0] + ';' + entry.ALIAS
     reg_players.loc[reg_players['Name'] == entry.Name, ['CLUB' + str(occurrencies[entry.Name])]] = entry.ALIAS
 
+# get firstnames and surnames
 names = list(reg_players['Name'])
 
 surnames = []
@@ -56,6 +61,7 @@ for name in names:
     oebv_entry = oebv_players[oebv_players['Name'] == name.lower()].values
 
     if len(oebv_entry) > 0:
+        # if existing from the ÖBV data
         if convert_uml:
             firstnams.append(convert(oebv_entry[0, 1]).title())
             surnames.append(convert(oebv_entry[0, 0]).title())
@@ -63,6 +69,7 @@ for name in names:
             firstnams.append(oebv_entry[0, 1].title())
             surnames.append(oebv_entry[0, 0].title())
     else:
+        # else by processing the names
         if convert_uml:
             name = convert(name)
 
@@ -71,6 +78,7 @@ for name in names:
         surnames.append(tmp[-1])
         firstnams.append(' '.join(tmp[0:-1]))
 
+# prepare for export
 reg_players['SURNAME'] = surnames
 reg_players['FIRSTNAME'] = firstnams
 reg_players['STATUS'] = 'Member'
@@ -78,8 +86,5 @@ reg_players['STATUS'] = 'Member'
 reg_players.drop(columns=['Nick', 'Name'], inplace=True)
 # reg_players = reg_players.head(10)
 
+# export
 reg_players.to_csv('bridgewebs_import.csv', encoding='iso 8859-1', index=False)
-
-# print(reg_players.head())
-
-
