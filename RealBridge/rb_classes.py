46ca6098dfa99e757d5b1048bf9ae1c51eda677b
Christian Bernscherer <cbernscherer@gmail.com>
2020-12-31 10:00:36 +0100

    round robin

--- RealBridge/rb_classes.py
+++ RealBridge/rb_classes.py
@@ -2,3 +2,16 @@ class Player():
     def __init__(self, pl_name, pl_id=0):
         self.name = pl_name
         self.id = pl_id
+
+class Pair():
+    def __init__(self, players, id):
+        self.players = players
+        self.id = id
+
+class Team():
+    def __init__(self, team_id, team_name):
+        self.id = team_id
+        self.name = team_name
+
+        self.players = []
+        self.pairs = []
--- RealBridge/team_round_robin.py
+++ RealBridge/team_round_robin.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from rb_classes import Player
+from rb_classes import Player, Pair, Team
 
 with open('201227121341TeamforAustria27122020_lin.xml', 'r') as f:
     t = f.read()
@@ -13,8 +13,11 @@ participants = soup.find('PARTICIPANTS')
 teamssoup = participants.findAll('TEAM')
 
 teams = []
-for team in teamssoup:
-    t = {}
-    t['Nr'] =team.attrs['TEAM_ID']
-    t['Name'] = team.attrs['TEAM_NAME']
-    teams.append(t)
+for ts in teamssoup:
+    attrs = ts.attrs
+    team = Team(attrs['TEAM_ID'], attrs['TEAM_NAME'])
+
+    teams.append(team)
+
+for team in teams:
+    print(team.id, team.name)
