042fcfe50a95cb56c6385550e68358633f2f8123
Christian Bernscherer <cbernscherer@gmail.com>
2021-01-03 14:22:10 +0100

    2021 01 03-2

--- RealBridge/extract_names.py
+++ RealBridge/extract_names.py
@@ -1,10 +1,25 @@
 import re
 
 config = 'd:\\Users\\cbern\\Desktop\\dkpsettings.json'
+participants ='participants.txt'
 
-with open(config, 'r') as f:
+with open(config, 'r', encoding='utf-8') as f:
     conf = f.read()
 
 conf = conf[conf.find('seat_assignments')-1:]
+# print(conf)
 
+search_for = r'\w+ \w+'
+match = re.compile(search_for)
 
+names = match.findall(conf)
+
+with open(participants, 'r', encoding='utf-8') as f:
+    partic_exist = f.read()
+
+with open(participants, 'a', encoding='utf-8') as f:
+    for name in names:
+        if partic_exist.find(name) != -1:
+            continue
+
+        f.write(name + ',\n')
