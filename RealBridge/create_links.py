5748a0720a72b06bbe7c8e07ee1a725ac8e5454e
Christian Bernscherer <cbernscherer@gmail.com>
2021-01-05 14:17:44 +0100

    2021 01 05

--- RealBridge/create_links.py
+++ RealBridge/create_links.py
@@ -5,7 +5,7 @@ link_file = 'links.txt'
 with open(participants, 'r', encoding='utf-8') as f:
     players = f.read().split('\n')
 
-players = [p.split(',') for p in players[1:]]
+players = [p.split(',') for p in players]
 
 with open(link_file, 'w', encoding='utf-8') as f:
     for player in players:
--- RealBridge/extract_names.py
+++ RealBridge/extract_names.py
@@ -1,11 +1,16 @@
 import json
+import pandas as pd
+import numpy as np
 
 directions = ['n', 's', 'e', 'w']
 config = 'd:\\Users\\cbern\\Desktop\\dkpsettings.json'
 participants ='participants.txt'
+member_file = 'SpoXls.xls'
 
 with open(config, 'r', encoding='utf-8') as f:
     conf = json.load(f)
+
+members = pd.read_excel(member_file)
 
 tables = []
 if 'seat_assignments' in conf:
@@ -23,6 +28,37 @@ with open(participants, 'r', encoding='utf-8') as f:
 with open(participants, 'a', encoding='utf-8') as f:
     for name in names:
         if partic_exist.find(name) != -1:
+            # print(1)
             continue
 
-        f.write(name + ',\n')
+        name_parts = name.split(' ')
+        last_name = name_parts[-1].upper()
+        first_name = ' '.join(name_parts[:-1])
+
+        oebv_nr = ''
+
+        extract_members = members[(members['NAME'] == last_name) & (members['VNAME'] == first_name)]
+        if len(extract_members) == 1:
+            oebv_nr = int(extract_members['NR'])
+
+        else:
+            extract_members = members[members['NAME'] == last_name]
+
+            first_names_found = extract_members['VNAME'].values
+
+            matches = []
+            for fnf in first_names_found:
+                matches.append(first_name.lower() in str(fnf).lower())
+
+            if sum(matches) == 1:
+                oebv_nr = int((extract_members[matches])['NR'])
+
+            else:
+                # no firstname match
+                if len(extract_members) == 1:
+                    if input('In den ÖBV Daten gefunden: {} {}; Name in der Sitzordnung: {}; Zuordnen(j/n)'.format(
+                        str(extract_members['VNAME'].values[0]), str(extract_members['NAME'].values[0]), name)) == 'j':
+
+                        oebv_nr = int((extract_members)['NR'])
+
+        f.write('{},{}\n'.format(name, str(oebv_nr)))
