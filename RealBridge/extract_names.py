cbbb327f0608bfd6f0107dfded242b1854baddae
Christian Bernscherer <cbernscherer@gmail.com>
2021-01-05 10:13:28 +0100

    2021 01 03-2

--- RealBridge/extract_names.py
+++ RealBridge/extract_names.py
@@ -1,18 +1,21 @@
-import re
+import json
 
+directions = ['n', 's', 'e', 'w']
 config = 'd:\\Users\\cbern\\Desktop\\dkpsettings.json'
 participants ='participants.txt'
 
 with open(config, 'r', encoding='utf-8') as f:
-    conf = f.read()
+    conf = json.load(f)
 
-conf = conf[conf.find('seat_assignments')-1:]
-# print(conf)
+tables = []
+if 'seat_assignments' in conf:
+    tables = conf['seat_assignments']
 
-search_for = r'\w+ \w+'
-match = re.compile(search_for)
-
-names = match.findall(conf)
+names = []
+for table in tables:
+    for direction in directions:
+        if direction in table:
+            names.append(table[direction])
 
 with open(participants, 'r', encoding='utf-8') as f:
     partic_exist = f.read()
