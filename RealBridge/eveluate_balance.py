8d7bb9457ec79d81bed73c49b8a908a4fb4f3236
Christian Bernscherer <cbernscherer@gmail.com>
2021-01-20 09:38:40 +0100

    2021 01 20

--- RealBridge/eveluate_balance.py
+++ RealBridge/eveluate_balance.py
@@ -1,7 +1,8 @@
 import bs4
 import numpy as np
 
-usebio = '210102122486Dreiknigspokal2021.xml'
+# usebio = '210102122486Dreiknigspokal2021.xml'
+usebio = '210119125250Dienstag1912021.xml'
 
 with open(usebio, 'r', encoding='utf-8') as f:
     soup = bs4.BeautifulSoup(f, 'xml')
@@ -22,10 +23,13 @@ s_opt = nr_tables * (nr_tables - 1) * nr_boards
 bal_matrix = np.zeros([nr_pairs, nr_pairs], dtype=np.int)
 
 boards = soup.findAll('BOARD')
+movement = []
 
 for board in boards:
     ns = [int(n.text)-1 for n in board.findAll('NS_PAIR_NUMBER')]
     ew = [int(n.text)-1 for n in board.findAll('EW_PAIR_NUMBER')]
+
+    movement.append([(ns[i]+1, ew[i]+1) for i in range(0, len(ns))])
 
     for i in range(0, len(ns)):
         bal_matrix[min(ns[i], ew[i]), max(ns[i], ew[i])] += len(ns) - 1
@@ -39,7 +43,11 @@ for board in boards:
             bal_matrix[min(ns[i], ns[j]), max(ns[i], ns[j])] += 1
             bal_matrix[min(ew[i], ew[j]), max(ew[i], ew[j])] += 1
 
-bal_matrix = bal_matrix ** 2
+bal_matrix_sq = bal_matrix ** 2
 
-calibre = 100. * s_opt**2 / (.5 * nr_pairs * (nr_pairs - 1) * np.sum(bal_matrix))
-print('calibre:', round(calibre))
+calibre = 100. * s_opt**2 / (.5 * nr_pairs * (nr_pairs - 1) * np.sum(bal_matrix_sq))
+
+for i in range(0, nr_boards, 2):
+    print(movement[i])
+
+print('calibre:', round(calibre, 2))
